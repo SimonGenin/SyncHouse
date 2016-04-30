578034cee2c6fb193518274cf18fba509adec2f4
Simon Genin <simongenin@hotmail.fr>
2016-04-30 22:36:01 +0200

    Changes in GCM

--- app/src/main/java/be/simongenin/synchouse/SyncHouseApplication.java
+++ app/src/main/java/be/simongenin/synchouse/SyncHouseApplication.java
@@ -1,6 +1,5 @@
 package be.simongenin.synchouse;
 
-
 import android.app.Application;
 
 import com.android.volley.RequestQueue;
@@ -10,8 +9,7 @@ import be.simongenin.synchouse.requests.OkHttpStack;
 
 public class SyncHouseApplication extends Application {
 
+    // The request queue for the application
     public RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext(), new OkHttpStack());
-
-
 
 }
